aa0d31755462bf2e3b024119f5fc1660c75e5766
kristi8 <qristina.chelidze@gmail.com>
2022-11-13 21:54:05 +0400

    js

--- script.js
+++ script.js
@@ -8,3 +8,35 @@ burgerbar.addEventListener('click', function() {
     burger.classList.toggle('activebar')
 
 })
+
+// 2
+
+
+let input = document.querySelector('.input-form');
+let addButton = document.querySelector('.btn');
+let ul = document.querySelector('.ul-items');
+let clearall = document.querySelector('clear')
+
+addButton.addEventListener('click' , function(){
+    let inputValue = input.value;
+    if (inputValue == " "){
+        return;
+    }
+    let li = document.createElement('li');
+    let newclean = document.createElement('button');
+    newclean.textContent = 'delete';
+    newclean.addEventListener('click' , function() {
+        li.remove();
+    })
+    
+
+
+    li.innerText = inputValue;
+
+    li.appendChild( newclean);
+    ul.appendChild(li);
+    input.value = ' ';
+})
+// clearall.addEventListener('click' , function() {
+//     ul.innerHTML = " ";
+// })

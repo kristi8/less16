4689bc24a5b0e8bba733e26fad58a176b273fc4e
kristi8 <qristina.chelidze@gmail.com>
2022-11-15 22:19:38 +0400

    add "clear'

--- script.js
+++ script.js
@@ -1,42 +1,46 @@
-let navigation = document.getElementById ('navbar');
-let burgerbar = document.getElementById ('burgerBar');
-let burger = document.getElementById ('burgerBar');
+"use strict";
 
+let navigation = document.getElementById("navbar");
+let burgerbar = document.getElementById("burgerBar");
+let burger = document.getElementById("burgerBar");
 
-burgerbar.addEventListener('click', function() {
-    navigation.classList.toggle('activenav')
-    burger.classList.toggle('activebar')
-
-})
+burgerbar.addEventListener("click", function () {
+  navigation.classList.toggle("activenav");
+  burger.classList.toggle("activebar");
+});
 
 // 2
 
+// let form = document.querySelector('.form')
+let input = document.querySelector(".input-form");
+let addButton = document.querySelector(".btn");
+let ul = document.querySelector(".ul-items");
+let clearall = document.querySelector(".clear");
+// let form = document.querySelector(".form-wraper");
 
-let input = document.querySelector('.input-form');
-let addButton = document.querySelector('.btn');
-let ul = document.querySelector('.ul-items');
-let clearall = document.querySelector('clear')
-
-addButton.addEventListener('click' , function(){
-    let inputValue = input.value;
-    if (inputValue == " "){
-        return;
-    }
-    let li = document.createElement('li');
-    let newclean = document.createElement('button');
-    newclean.textContent = 'delete';
-    newclean.addEventListener('click' , function() {
-        li.remove();
-    })
-    
-
-
-    li.innerText = inputValue;
-
-    li.appendChild( newclean);
-    ul.appendChild(li);
-    input.value = ' ';
-})
-// clearall.addEventListener('click' , function() {
-//     ul.innerHTML = " ";
+// form.addEventListener('submit', function(event){
+//     event.preventDefault();
 // })
+
+addButton.addEventListener("click", function () {
+  let inputValue = input.value;
+  if (inputValue == " ") {
+    return;
+  }
+  let li = document.createElement("li");
+  let newclean = document.createElement("button");
+  newclean.textContent = "delete";
+  newclean.addEventListener("click", function () {
+    li.remove();
+  });
+
+  li.innerText = inputValue;
+
+  li.appendChild(newclean);
+  ul.appendChild(li);
+  input.value = " ";
+});
+
+clearall.addEventListener("click", function () {
+  ul.innerHTML = " ";
+});
